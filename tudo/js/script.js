5421e00b3993f1c39727cd8dfc4dd40cb50bebc6
Vitor Farias <103939291+vitorglfarias@users.noreply.github.com>
2023-01-30 16:52:11 -0300

    Add files via upload

---
+++ tudo/js/script.js
@@ -0,0 +1,7 @@
+/* const botao = document.getElementById('botao');
+const informacoes = document.querySelector('#informacoes');
+
+botao.addEventListener("click", function(){
+    informacoes.classList.remove('informacoes-off');
+    informacoes.classList.add('informacoes-on');
+}) */
